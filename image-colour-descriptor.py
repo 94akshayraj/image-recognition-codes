da0311b4008ece5838c9ff1ab393b6231adbe694
Akshay. R <akhisreelibra@gmail.com>
2019-11-22 11:33:51 +0530

    Create image-colour-descriptor.py

---
+++ image-colour-descriptor.py
@@ -0,0 +1,11 @@
+from sklearn.cluster import KMeans
+import cv2
+def process_image_clusters(input_image,num_clusters):
+    image = cv2.imread(input_image)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    image = image.reshape((image.shape[0] * image.shape[1], 3))
+
+    clt = KMeans(n_clusters = num_clusters)
+    clt.fit(image)
+    return clt.cluster_centers_
